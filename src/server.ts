246b76a0204addf491bd8d284ccc8a99b46a539e
MariaJulia <mariajuliadev@gmail.com>
2026-07-26 12:45:54 -0300

    Alterações do deploy

--- src/server.ts
+++ src/server.ts
@@ -3,25 +3,32 @@ import cors from "cors";
 import { router } from "./routes";
 
 const app = express();
+
 app.use(express.json());
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    optionsSuccessStatus: 200
-}));
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+        optionsSuccessStatus: 200,
+    })
+);
+
 app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Error) {
         return res.status(400).json({
-            error: err.message
-        })
+            error: err.message,
+        });
     }
 
     return res.status(500).json({
         status: "error",
-        message: "Internal Server Error."
-    })
-})
+        message: "Internal Server Error.",
+    });
+});
 
-app.listen(3000, () => console.log("Server running on port 3000."))
+const port = Number(process.env.PORT) || 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}.`));
