005afca0174a0600ab1bbe11313873014b5a1a14
MariaJulia <mariajuliadev@gmail.com>
2026-07-26 13:09:17 -0300

    Atualiza o CORS

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://my-workouts-frontend-six.vercel.app",
         credentials: true,
         optionsSuccessStatus: 200,
     })
